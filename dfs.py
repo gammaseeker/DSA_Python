fa77fb6cb9fc92cc52de4bc5d0518931fe9b5382
Joey <jjiemjitpolchai9540@bths.edu>
2019-10-08 14:44:36 -0400

    recursive matrix

--- dfs.py
+++ dfs.py
@@ -19,6 +19,21 @@ def dfs(matrix):
         else:
             col += 1
 
+def dfs_recursive(matrix, i, j):
+    if matrix[i][j] == 1:
+        print("{}, {}".format(i, j))
+        matrix[i][j] = 0
+    else:
+        return
+    if i - 1 >= 0:
+        dfs_recursive(matrix, i-1, j)
+    if i + 1 < len(matrix):
+        dfs_recursive(matrix, i+1, j)
+    if j - 1 >= 0:
+        dfs_recursive(matrix, i, j-1)
+    if j + 1 < len(matrix[i]):
+        dfs_recursive(matrix, i, j + 1)
+
 matrix = [
     [0, 1, 1, 0, 0, 0, 0], 
     [1, 0, 0, 1, 1, 0, 0], 
@@ -35,4 +50,11 @@ simple_matrix = [
     [1, 0, 0]
 ]
 
+rec_matrix = [
+    [1, 1, 0],
+    [1, 1, 1],
+    [0, 0, 1]
+]
+
 dfs(simple_matrix)
+dfs_recursive(rec_matrix, 0, 0)
