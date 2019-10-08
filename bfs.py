c9da548a5bd7b03abcc275801ec2a40f0ff464ac
Joey <jjiemjitpolchai9540@bths.edu>
2019-10-08 14:44:23 -0400

    more test cases

--- bfs.py
+++ bfs.py
@@ -1,25 +1,17 @@
+import collections
+
 def bfs(matrix):
     visited = set()
-    queue = []
-    visited.add(0)
-    for i in range(0, len(matrix[0])):
-        if matrix[0][i] == 1:
-            queue.append(i)
-
-    row = queue.pop(0)
-    col = 0
-
+    queue = collections.deque([0])
     while queue:
-        if matrix[row][col] == 1:
-            if col not in visited:
-                queue.append(col)
-        if col == len(matrix[row]) - 1:
-            print(row)
-            visited.add(row)
-            row = queue.pop(0)
-            col = 0
-        else:
-            col += 1
+        row = queue.popleft()
+        for col in range(0, len(matrix[row])):
+            if matrix[row][col] == 1:
+                if col not in visited:
+                    queue.append(col)
+            if col == len(matrix[row]) - 1:
+                print(row)
+                visited.add(row)
 
 simple_matrix = [
     [0, 1, 1],
@@ -27,4 +19,16 @@ simple_matrix = [
     [1, 0, 0]
 ]
 
-bfs(simple_matrix)
+another_matrix = [
+    [0, 1, 1, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 1, 1, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0]
+]
+bfs(simple_matrix) # 0, 1, 2
+bfs(another_matrix) # 0, 1, 2, 3, 4, 5, 6, 7, 8
