10677d72d2415e91a93313f764c757675218d195
Joey <jjiemjitpolchai9540@bths.edu>
2019-10-20 10:53:15 -0400

    reverse reverse!

--- linked_list/linkedlist.py
+++ linked_list/linkedlist.py
@@ -50,6 +50,22 @@ class LinkedList:
             print(current)
             current = current.next
 
+def reverse(head):
+    current = head
+    prev = None
+    while current:
+        next = current.next
+        current.next = prev
+        prev = current
+        current = next
+    return prev
+
+def print_list_from_node(head):
+    current = head
+    while current:
+        print(current.value)
+        current = current.next
+
 node1 = Node(1)
 node2 = Node(2)
 linkedList = LinkedList()
@@ -57,3 +73,13 @@ linkedList.set_head(node1)
 linkedList.set_tail(node1)
 linkedList.add(node2)
 linkedList.print_list()
+
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+node1.next = node2
+node2.next = node3
+node3.next = node4
+rev = reverse(node1)
+print_list_from_node(rev)
